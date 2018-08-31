b2bbe9af362b09d5f6f6bf1b55f8c764bc615e0e
Dima Kostenich <dzka@splitmedialabs.com>
2018-08-31 16:30:35 +0300

    Static menu scene added

--- game/include/RenderObjectMenu.h
+++ game/include/RenderObjectMenu.h
@@ -4,6 +4,7 @@
 #include "IRenderObject.h"
 
 #include <memory>
+#include <string>
 
 #include "sdl/SDL.h"
 
@@ -19,7 +20,17 @@ public:
     bool Render() override;
 
 private:
+    const int m_iFontSize = 32;
+    const std::string m_strFontName;
+
+    SDL_Renderer* m_pRenderer;
+    SDL_Rect m_DrawingRect;
+
     std::unique_ptr<LTexture> m_pTextureBg;
+    std::unique_ptr<LTexture> m_pMenuItemSingle;
+    std::unique_ptr<LTexture> m_pMenuItemCreateLobby;
+    std::unique_ptr<LTexture> m_pMenuItemJoinLobby;
+    std::unique_ptr<LTexture> m_pMenuItemExit;
 };
 
 #endif // _RENDER_OBJECT_MENU_H_
--- game/src/RenderObjectMenu.cpp
+++ game/src/RenderObjectMenu.cpp
@@ -1,9 +1,17 @@
 #include "RenderObjectMenu.h"
 #include "LTexture.h"
 
+#include "logger/easylogging++.h"
+
 RenderObjectMenu::RenderObjectMenu(SDL_Renderer* pRenderer)
+    : m_pRenderer(pRenderer)
+    , m_strFontName("assets/sea-font.ttf")
 {
     m_pTextureBg = std::make_unique<LTexture>(pRenderer);
+    m_pMenuItemSingle = std::make_unique<LTexture>(pRenderer);
+    m_pMenuItemCreateLobby = std::make_unique<LTexture>(pRenderer);
+    m_pMenuItemJoinLobby = std::make_unique<LTexture>(pRenderer);
+    m_pMenuItemExit = std::make_unique<LTexture>(pRenderer);
 }
 
 RenderObjectMenu::~RenderObjectMenu()
@@ -13,12 +21,26 @@ RenderObjectMenu::~RenderObjectMenu()
 
 bool RenderObjectMenu::Initialize()
 {
-    return m_pTextureBg->LoadFromFile("assets/blue-burst-abstract-bg.png");
+    SDL_RenderGetViewport(m_pRenderer, &m_DrawingRect);
+    m_pTextureBg->LoadFromFile("assets/blue-burst-abstract-bg.png");
+
+    SDL_Color textColor = { 255, 255, 255 };
+    m_pMenuItemSingle->LoadFromRenderedText(m_strFontName.c_str(), m_iFontSize, "Single Player", textColor);
+    m_pMenuItemCreateLobby->LoadFromRenderedText(m_strFontName.c_str(), m_iFontSize, "Create Lobby", textColor);
+    m_pMenuItemJoinLobby->LoadFromRenderedText(m_strFontName.c_str(), m_iFontSize, "Join Lobby", textColor);
+    m_pMenuItemExit->LoadFromRenderedText(m_strFontName.c_str(), m_iFontSize, "Exit", textColor);
+
+    return true;
 }
 
 bool RenderObjectMenu::Render()
 {
     m_pTextureBg->Render(0, 0);
+
+    m_pMenuItemSingle->Render((m_DrawingRect.w - m_DrawingRect.x) / 2 - m_pMenuItemSingle->GetWidth() / 2, 150);
+    m_pMenuItemCreateLobby->Render((m_DrawingRect.w - m_DrawingRect.x) / 2 - m_pMenuItemSingle->GetWidth() / 2, 200);
+    m_pMenuItemJoinLobby->Render((m_DrawingRect.w - m_DrawingRect.x) / 2 - m_pMenuItemSingle->GetWidth() / 2, 250);
+    m_pMenuItemExit->Render((m_DrawingRect.w - m_DrawingRect.x) / 2 - m_pMenuItemSingle->GetWidth() / 2, 300);
 
     return true;
 }
