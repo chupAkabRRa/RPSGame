2dcc197d8e1306c13c07fd4ccdb78812614a24b1
Dima Kostenich <dzka@splitmedialabs.com>
2018-09-04 19:10:37 +0300

    Create bin/build path if needed

--- build_win_x64.bat
+++ build_win_x64.bat
@@ -1,3 +1,5 @@
+if not exist .\bin\win\x64 mkdir .\bin\win\x64
+if not exist .\build\win\x64 mkdir .\build\win\x64
 cd build\win\x64
 cmake -G "Visual Studio 14 2015 Win64" ..\..\..
 cmake --build . --config Release --target install
